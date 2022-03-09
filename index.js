a63e2a6e8e5757c68d0bfad856378009448ea9b2
sonamng <86510537+sonamng@users.noreply.github.com>
2022-03-09 10:38:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// var a="ssoonaamwww"
+// var i=a
+// s={}
+// for(var d=0;d<a.leanght;d++){
+//     if (s.inclueds (a)){
+//         s[i]+s[i]+1
+//     }
+//     else{
+//         s[i]=1
+//     }
+// }
+// console.log(s);
+
+
+
+// string = ""
+// const list1=[]
+// let output={}
+// for (var i of string) {
+//     if(list1.includes(i)){
+//         output[i]=output[i]+1
+//     }
+//     else{
+//         list1.push(i);
+//         output[i]=1;
+//     }
+// }
+// console.log(output);
+
+
